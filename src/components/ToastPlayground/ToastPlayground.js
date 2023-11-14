190a82a28cce6b060fe8e308da019d3f5306be4a
Jason <jasontwuk@gmail.com>
2023-11-14 20:29:32 +0000

    solution for exercise 2: Live-editable toast preview

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Button from '../Button';
+import Toast from '../Toast';
 
 import styles from './ToastPlayground.module.css';
 
@@ -9,6 +10,7 @@ const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 function ToastPlayground() {
   const [message, setMessage] = React.useState("");
   const [variant, setvariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [isToastOpen, setIsToaskOpen] = React.useState(false);
 
   return (
     <div className={styles.wrapper}>
@@ -17,6 +19,12 @@ function ToastPlayground() {
         <h1>Toast Playground</h1>
       </header>
 
+      {isToastOpen && (
+        <Toast variant={variant} setIsToaskOpen={setIsToaskOpen}>
+          {message}
+        </Toast>
+      )}
+      
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
           <label
@@ -61,7 +69,7 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <Button>Pop Toast!</Button>
+            <Button onClick={() => setIsToaskOpen(true)}>Pop Toast!</Button>
           </div>
         </div>
       </div>
